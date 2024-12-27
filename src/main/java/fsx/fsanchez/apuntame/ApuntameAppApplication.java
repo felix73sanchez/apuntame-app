cf34ec30b5bf90e14562e8b4ab6fe2a6e1e2d9fd
Felix Sanchez <felixsanchez73@outlook.com>
2024-12-27 16:56:12 -0400

    Empezando proyecto Apuntame-app

---
+++ src/main/java/fsx/fsanchez/apuntame/ApuntameAppApplication.java
@@ -0,0 +1,13 @@
+package fsx.fsanchez.apuntame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApuntameAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApuntameAppApplication.class, args);
+    }
+
+}
